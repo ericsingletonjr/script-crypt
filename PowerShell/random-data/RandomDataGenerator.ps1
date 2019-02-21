864def937259262c54f4ebdcd1f3b949b12f7965
Eric Singleton <eric.singleton.jr@gmail.com>
2019-02-21 12:16:19 -0800

    Added headerName creation and file checking

--- PowerShell/random-data/RandomDataGenerator.ps1
+++ PowerShell/random-data/RandomDataGenerator.ps1
@@ -5,7 +5,45 @@ function Get-ScriptDirectory
 
 $path = (Get-ScriptDirectory)
 
-$employees = @(
+$FileExist = (Test-Path -Path $path'\headers.csv')
+
+if(!$FileExist)
+{
+    $headerCreate = @(
+        [PSCustomObject]@{
+            Name = 'Error'
+        }
+        [PSCustomObject]@{
+            Name = 'MachineName'
+        }
+        [PSCustomObject]@{
+            Name = 'ID'
+        }
+        [PSCustomObject]@{
+            Name = 'Memory'
+        }
+        [PSCustomObject]@{
+            Name = 'Space'
+        }
+        [PSCustomObject]@{
+            Name = 'Event'
+        }
+        [PSCustomObject]@{
+            Name = 'Message'
+        }
+        [PSCustomObject]@{
+            Name = 'Level'
+        }
+    )
+    $headerCreate | Export-Csv -Path $path'\headers.csv' -NoTypeInformation
+}
+else 
+{
+    Write-Host 'Already exists...'
+    $headerNames = Import-Csv -Path $path'\headers.csv'
+}
+$headerNames
+$seedData = @(
     [PSCustomObject]@{
         FristName = 'Adam'
         LastName = 'Bertram'
@@ -18,4 +56,4 @@ $employees = @(
     }
 )
 
-$employees | Export-Csv -Path $path'\seed.csv' -NoTypeInformation
+$seedData | Export-Csv -Path $path'\seed.csv' -NoTypeInformation
